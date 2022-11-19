3ffdecefa8841dc4ae7103c939cb388398e1e58c
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-19 15:18:31 +0300

    Create design_algo.c

---
+++ AssessmentWeek/design_algo.c
@@ -0,0 +1,13 @@
+//Design an algorithm OR draw a flowchart for a computer program
+
+1. BEGIN
+2. PRINT “Enter three numbers:”
+3. READ num1, num2, num3
+4. average = (num1+num2+num3)/3
+5. IF average >= 50
+PRINT “pass”
+ELSE
+        PRINT “fail”
+ENDIF
+6. PRINT “average:”, average
+7. END
