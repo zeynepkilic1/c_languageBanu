a5cd8ba5b074224d2a084a37ed6e5524a3ef871a
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-28 11:18:05 +0300

    calculator using struct and union
    the bug is solved

--- Calc_use_struct.c
+++ Calc_use_struct.c
@@ -1,16 +1,24 @@
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "ctype.h" // for isdigit() 
+#include "ctype.h" // for isdigit()
+
+union Data {
+    int i;
+    float f;
+    char str[20];
+};
 
 struct calculator {
     int choice;
     int howmanynumbers;
-    int numbers[howmanynumbers];
+
     int result;
 };
 
-int main() {
+int main(){
+    struct calculator cal;
     printf("Which operation do you want to perform? \n");
     printf("1. Addition \n");
     printf("2. Subtraction \n");
@@ -18,49 +26,46 @@ int main() {
     printf("4. Division \n");
     printf("5. Modulus \n");
     printf("6. Exit \n");
-    
-    struct calculator calc; // Here we create a variable of type calculator and name it calc.
     printf("Enter your choice: ");
-    scanf("%d", &calc.choice); // Here we access the choice variable of the calc variable.
-    
+    scanf("%d", &cal.choice);
     printf("How many numbers do you want to perform the operation on? ");
-    scanf("%d", &calc.howmanynumbers); // Here we access the howmanynumbers variable of the calc variable.
-   
-    for (int i = 0; i < calc.howmanynumbers; i++) {  // Here we use a for loop to access the numbers array of the calc variable.
+    scanf("%d", &cal.howmanynumbers);
+    union Data numbers[cal.howmanynumbers];
+    for (int i = 0; i < cal.howmanynumbers; i++) {
         printf("Enter number %d: ", i + 1);
-        scanf("%d", &calc.numbers[i]);
+        scanf("%d", &numbers[i].i);
     }
-    calc.result = calc.numbers[0];
-    switch (calc.choice) {
+    cal.result = numbers[0].i;
+    switch (cal.choice) {
         case 1:
-            for (int i = 1; i < calc.howmanynumbers; i++) {
-                calc.result += calc.numbers[i];
+            for (int i = 1; i < cal.howmanynumbers; i++) {
+                cal.result += numbers[i].i;
             }
-            printf("The result is %d", calc.result);
+            printf("The result is %d", cal.result);
             break;
         case 2:
-            for (int i = 1; i < calc.howmanynumbers; i++) {
-                calc.result -= calc.numbers[i];
+            for (int i = 1; i < cal.howmanynumbers; i++) {
+                cal.result -= numbers[i].i;
             }
-            printf("The result is %d", calc.result);
+            printf("The result is %d", cal.result);
             break;
         case 3:
-            for (int i = 1; i < calc.howmanynumbers; i++) {
-                calc.result *= calc.numbers[i];
+            for (int i = 1; i < cal.howmanynumbers; i++) {
+                cal.result *= numbers[i].i;
             }
-            printf("The result is %d", calc.result);
+            printf("The result is %d", cal.result);
             break;
         case 4:
-            for (int i = 1; i < calc.howmanynumbers; i++) {
-                calc.result /= calc.numbers[i];
+            for (int i = 1; i < cal.howmanynumbers; i++) {
+                cal.result /= numbers[i].i;
             }
-            printf("The result is %d", calc.result);
+            printf("The result is %d", cal.result);
             break;
         case 5:
-            for (int i = 1; i < calc.howmanynumbers; i++) {
-                calc.result %= calc.numbers[i];
+            for (int i = 1; i < cal.howmanynumbers; i++) {
+                cal.result %= numbers[i].i;
             }
-            printf("The result is %d", calc.result);
+            printf("The result is %d", cal.result);
             break;
         case 6:
             exit(0);
@@ -69,5 +74,3 @@ int main() {
     }
     return 0;
 }
-
-
