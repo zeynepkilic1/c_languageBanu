5710ce707f90112c21155332e98e918ada802064
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-11-29 13:08:19 +0300

    Version 2 solved

--- username_pass_func.c
+++ username_pass_func.c
@@ -1,4 +1,27 @@
-//create a username and password and check it with function
+// //create a username and password and check it with function
+
+// #include "stdio.h"
+// #include "stdlib.h"
+// #include "string.h"
+// #include "ctype.h" // for isdigit()
+
+// void login(char username[], char password[]) {
+// //   your code here  
+// //   code burada
+  
+// }
+
+// int main() {
+//     char username[20];
+//     char password[20];
+//     printf("Enter your username: ");
+//     scanf("%s", username);
+//     printf("Enter your password: ");
+//     scanf("%s", password);
+//     login(username, password);
+//     return 0;
+// }
+
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -6,9 +29,18 @@
 #include "ctype.h" // for isdigit()
 
 void login(char username[], char password[]) {
-//   your code here  
-//   code burada
-  
+    char username1[20];
+    char password1[20];
+    printf("Enter your username: ");
+    scanf("%s", username1);
+    printf("Enter your password: ");
+    scanf("%s", password1);
+    if (strcmp(username, username1) == 0 && strcmp(password, password1) == 0) {
+        printf("You are logged in");
+    } else {
+        printf("Wrong username or password");
+    }
+
 }
 
 int main() {
