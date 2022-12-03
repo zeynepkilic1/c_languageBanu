5c5410aa723a8e4a0c2183162410550968e637c8
zeynepkilic1 <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-03 22:26:52 +0300

    execise_complete.c
    zeynep kılıç

--- chapter_9.3/execise_complete.c
+++ chapter_9.3/execise_complete.c
@@ -1,13 +1,20 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include"math.h"
+
+int main(void)
+{
+    double floor;
+    int power;
+
+    printf("Enter a number:");
+    scanf("%lf",&floor);
+    printf("Enter a power:");
+    scanf("%d",&power);
+
+    float result=pow(floor,power);
+    printf("result:%.3f\n",result);
 
 
-class Solution{
-//     your code here
-        public:
-        double myPow(double x, int n) {
+    return 0;
+}
 
-        }
-};
-
-//     your code here
