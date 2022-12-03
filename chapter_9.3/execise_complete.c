3e415e4ffcb5f259c72a4fc13a58816a85610837
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-03 19:54:56 +0300

    Create execise_complete.c

---
+++ chapter_9.3/execise_complete.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+
+class Solution{
+//     your code here
+        public:
+        double myPow(double x, int n) {
+
+        }
+};
+
+//     your code here
