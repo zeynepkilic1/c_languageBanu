fb4868b7a7c6ffaec6eaea79e1b8433957a9a1e3
zeynepkilic1 <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-03 22:20:29 +0300

    array_reverse.c
    zeynep kılıç

--- chapter_9.2/array_reverse.c
+++ chapter_9.2/array_reverse.c
@@ -3,18 +3,23 @@
 
 int main()
 {
-    int number, *arrays, i;
-    scanf("%d", &number);
-    arrays = (int*) malloc(number * sizeof(int));
-    for(i = 0; i < number; i++) {
-        scanf("%d", arrays + i);
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
     }
 
 
-    /* Write the logic to reverse the array. */
-    //    burada array tersi alınacak ve yazdırılacak
+    int temp;
+    for (i = 0; i < num / 2; i++) {
+        temp = (int) *(arr + num - i - 1);
+        *(arr + num - i - 1) = *(arr + i);
+        *(arr + i) = temp;
+    }   
 
-    for(i = 0; i < number; i++)
-        printf("%d ", *(arrays + i));
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
     return 0;
 }
+
