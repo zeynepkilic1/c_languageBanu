23de8217c2c775b95508357044e4a82660cc7daf
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-05 11:05:41 +0300

    Create operators.c

---
+++ chapter_9.3/operators.c
@@ -0,0 +1,14 @@
+//complete the codes
+
+
+#include <stdio.h>
+int main()
+{
+    int a = 9,b = 4, c;
+    
+  //your code here 
+  
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
+}
