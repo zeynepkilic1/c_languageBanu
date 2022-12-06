d05927e3252f6d78697dd750ea4f9f9098823ee7
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-06 14:29:49 +0300

    Update for Loopwitharray.c

--- Odevler/for Loopwitharray.c
+++ Odevler/for Loopwitharray.c
@@ -1,8 +1,6 @@
-// For loop with 2D array myarray[2][2]
-
-// without for loop 
 
 #include "stdio.h"
+
 int main(){
     int myarray[9][9] = {
             {1, 2, 3, 4, 5, 6, 7, 8, 9},
@@ -15,11 +13,28 @@ int main(){
             {8, 16, 24, 32, 40, 48, 56, 64, 72},
             {9, 18, 27, 36, 45, 54, 63, 72, 81}
     };
-    
+
     printf("myarray[0][0]=%d\n",myarray[0][0]);
     printf("myarray[0][1]=%d\n",myarray[0][1]);
     printf("myarray[1][0]=%d\n",myarray[1][0]);
     printf("myarray[1][1]=%d\n",myarray[1][1]);
+    printf("myarray[2][2]=%d\n",myarray[2][2]);
+    printf("myarray[2][3]=%d\n",myarray[2][3]);
+    printf("myarray[3][2]=%d\n",myarray[3][2]);
+    printf("myarray[3][3]=%d\n",myarray[3][3]);
+    printf("myarray[4][4]=%d\n",myarray[4][4]);
+    printf("myarray[4][5]=%d\n",myarray[4][5]);
+    printf("myarray[5][4]=%d\n",myarray[5][4]);
+    printf("myarray[5][5]=%d\n",myarray[5][5]);
+    printf("myarray[6][6]=%d\n",myarray[6][6]);
+    printf("myarray[6][7]=%d\n",myarray[6][7]);
+    printf("myarray[7][6]=%d\n",myarray[7][6]);
+    printf("myarray[7][7]=%d\n",myarray[7][7]);
+    printf("myarray[8][8]=%d\n",myarray[8][8]);
+    printf("myarray[8][7]=%d\n",myarray[8][7]);
+    printf("myarray[7][8]=%d\n",myarray[7][8]);
+    printf("myarray[7][7]=%d\n",myarray[7][7]);
     return 0;
 }
+    
 
