65abd8660deb075c76820cdac819ca17b6b59d32
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-06 13:36:19 +0300

    Update comapre.c
    Hata ayiklandi

--- Odevler/comapre.c
+++ Odevler/comapre.c
@@ -10,19 +10,26 @@
 
 
 int main() {
-    printf("Enter your name :");
-    scanf("%s", name);
-    printf("Enter your age :");
-    scanf("%d", age);
-    printf("Enter your favnum :");
-    scanf("%f", favnum);
-
-
-    if (strcmp(name, "Akif") == 0 && age == 32 && favnum == 3.14) {
+//    compare my name and age and favnum 
+    printf("Enter your name: ");
+    char name1[20];
+    scanf("%s", name1);
+    printf("Enter your age: ");
+    int age1;
+    scanf("%d", &age1);
+    printf("Enter your favnum: ");
+    float favnum1;
+    scanf("%f", &favnum1);
+if (strcmp(name, name1) == 0 && age == age1 && favnum == favnum1) {
         printf("You are Akif, I know who you are man, my favorite teacher");
     } else {
-        printf("You are not Akif, I don't know who you are as I am not that clever");
-
-        return 0;
-
+         printf("You are not Akif, I don't know who you are as I am not that clever");
+        
     }
+    return 0;
+}
+
+
+
+
+
