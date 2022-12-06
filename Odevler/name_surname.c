24a57399731b610de96a299a341e22fbc47af192
Zeynep Kılıç <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-06 13:13:18 +0300

    Update name_surname.c
    @themanoftalent Zeynep kılıc

--- Odevler/name_surname.c
+++ Odevler/name_surname.c
@@ -1,1 +1,14 @@
-// Display your name and surname on screen 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char name[20] = "zeynep";
+    char surname[20] = "kilic";
+
+    printf("enter your name: %s\n",name);
+
+    printf("enter your surname: %s",surname);
+
+    return 0;
+}
