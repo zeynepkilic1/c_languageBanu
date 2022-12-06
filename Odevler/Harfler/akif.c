5c3bbd1e6eb6e8a6212df4d01b00e6c4364691c8
Zeynep Kılıç <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-07 01:47:02 +0300

    zeynep kılıç
    @themanoftalent 
    zeynep kılıç
    akif.png project
    
    int main()
    {
        int i, j;
    
        char name1='A';
        char name2='k';
        char name3='i';
        char name4='f';
       
        for(i=0; i<=5; i++)
        {
            for(j=1; j<(2*5-2*i); j++)
            {
                printf(" ");
            }
            printf(" ");
            for(j=0; j<i; j++)
            {
                printf("%c",name1);
            }
            
            for(j=i; j<5; j++)
            {
                printf("");
            }
            printf(" ");
            for(j=0; j<i; j++)
            {
              printf("%c",name2);  
            }
            printf(" ");
            for(j=0; j<i; j++)
            {
                printf("%c",name3);
            }
            for(j=0; j<i; j++)
            {
                printf("");
            }
            printf(" ");
            for(j=0; j<i; j++)
            {
                printf("%c",name4);
            }
            printf("\n");
        
        }  
        
        
        return 0;
    }

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -1,4 +1,51 @@
-// Make your program write "Akif" in a traianlge shape. 
-// you have the code for ABC
-// imagine, think, search and do it.
-// Best luck!
+int main()
+{
+    int i, j;
+
+    char name1='A';
+    char name2='k';
+    char name3='i';
+    char name4='f';
+   
+    for(i=0; i<=5; i++)
+    {
+        for(j=1; j<(2*5-2*i); j++)
+        {
+            printf(" ");
+        }
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name1);
+        }
+        
+        for(j=i; j<5; j++)
+        {
+            printf("");
+        }
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+          printf("%c",name2);  
+        }
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name3);
+        }
+        for(j=0; j<i; j++)
+        {
+            printf("");
+        }
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name4);
+        }
+        printf("\n");
+    
+    }  
+    
+    
+    return 0;
+}
