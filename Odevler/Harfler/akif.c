066ef03d4874f774f3ac9905a0c2387a2c1e5689
Zeynep Kılıç <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-07 00:03:50 +0300

    Update akif.c
    @themanoftalent 
    zeynep kılıç
    //I did it two different ways. iI did the first with loop and second with array.....
    #include<stdio.h>
    #include<string.h>
    
    int main()
    {
        char name[]="AKIF";
        int i,j,row;
        row=strlen(name);
        
        for ( i = 0; i < row; i++)
        {
            for (j = 0; j <= i; j++)
            {
                printf("%c",name[j]);
            }
            printf("\n");
    
            
        }
      
        return 0;
    }
    
    
    //2--> ARRAY
    #include <stdio.h>
    
    int main(void)
    {
      char cdizi[] = "AKIF";
    
      printf("%.1s\n", cdizi);
      printf("%.2s\n", cdizi);
      printf("%.3s\n", cdizi);
      printf("%.4s\n", cdizi);
      
      return 0;
    }

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -1,4 +1,41 @@
-// Make your program write "Akif" in a traianlge shape. 
-// you have the code for ABC
-// imagine, think, search and do it.
-// Best luck!
+@themanoftalent 
+zeynep kılıç
+//I did it two different ways. iI did the first with loop and second with array.....
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char name[]="AKIF";
+    int i,j,row;
+    row=strlen(name);
+    
+    for ( i = 0; i < row; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf("%c",name[j]);
+        }
+        printf("\n");
+
+        
+    }
+  
+    return 0;
+}
+
+
+//2--> ARRAY
+#include <stdio.h>
+
+int main(void)
+{
+  char cdizi[] = "AKIF";
+
+  printf("%.1s\n", cdizi);
+  printf("%.2s\n", cdizi);
+  printf("%.3s\n", cdizi);
+  printf("%.4s\n", cdizi);
+  
+  return 0;
+}
