fa182627f9791e62e437cb5ecc273a4482126a33
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-06 14:18:03 +0300

    Update for Loopwitharray.c
    With and without for loop2D array

--- Odevler/for Loopwitharray.c
+++ Odevler/for Loopwitharray.c
@@ -1,1 +1,38 @@
 // For loop with 2D array myarray[2][2]
+
+// without for loop 
+
+#include "stdio.h"
+int main(){
+    int myarray[2][2] ={
+            {1,2},
+            {3,4}
+    };
+    printf("myarray[0][0]=%d\n",myarray[0][0]);
+    printf("myarray[0][1]=%d\n",myarray[0][1]);
+    printf("myarray[1][0]=%d\n",myarray[1][0]);
+    printf("myarray[1][1]=%d\n",myarray[1][1]);
+    return 0;
+}
+
+############################################################
+
+// with for loop 
+
+
+#include "stdio.h"
+
+int main(){
+    int myarray[2][2]= {
+            {1,2},
+            {3,4}
+    };
+   int i,j;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d",myarray[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
