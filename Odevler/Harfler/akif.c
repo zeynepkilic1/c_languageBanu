b2a2da58552dc7eb1cf59e387100b7c12eec348c
Zeynep Kılıç <119415722+zeynepkilic1@users.noreply.github.com>
2022-12-07 22:31:38 +0300

    zeynep kılıç
    New and correct version of akif.png
    @themanoftalent 
    zeynep kılıç
    
    #include <stdio.h>
    int main()
    {
        int i, j;
    
        char name1='A';
        char name2='k';
        char name3='i';
        char name4='f';
       
        for(i=0; i<=5; i++)
        {
            for(j=0; j<(5-i); j++)
            {
                printf(" ");
            }
            
            for(j=0; j<i; j++)
            {
                printf("%c",name1);
            }
            
            printf(" ");
            for(j=0; j<i; j++)
            {
              printf("%c",name2);  
            }
            printf(" ");
            for(j=0; j<i; j++)
            {
                printf("%c",name3);
            }
            
            printf(" ");
            for(j=0; j<i; j++)
            {
                printf("%c",name4);
            }
            printf("\n");
        
        }  
        
        
        return 0;
    }

--- Odevler/Harfler/akif.c
+++ Odevler/Harfler/akif.c
@@ -1,4 +1,45 @@
-// Make your program write "Akif" in a traianlge shape. 
-// you have the code for ABC
-// imagine, think, search and do it.
-// Best luck!
+#include <stdio.h>
+int main()
+{
+    int i, j;
+
+    char name1='A';
+    char name2='k';
+    char name3='i';
+    char name4='f';
+   
+    for(i=0; i<=5; i++)
+    {
+        for(j=0; j<(5-i); j++)
+        {
+            printf(" ");
+        }
+        
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name1);
+        }
+        
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+          printf("%c",name2);  
+        }
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name3);
+        }
+        
+        printf(" ");
+        for(j=0; j<i; j++)
+        {
+            printf("%c",name4);
+        }
+        printf("\n");
+    
+    }  
+    
+    
+    return 0;
+}
